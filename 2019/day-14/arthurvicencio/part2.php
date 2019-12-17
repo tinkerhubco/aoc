a024f01bd20e4c5c73baf5759f024894dd7bac39
arthurvicencio <artsvicencio@gmail.com>
2019-12-18 03:31:41 +0800

    Day 14

---
+++ 2019/day-14/arthurvicencio/part2.php
@@ -0,0 +1,177 @@
+<?php
+
+$input = '171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX';
+
+$input ='14 LQGXD, 6 TDLQ => 9 VGLV
+1 WBQF, 2 JZKMJ => 5 TRSK
+5 MGHZ, 5 ZLDQF => 8 HMVG
+1 JWQH, 1 QFBC, 2 ZXVNM => 8 JFJZH
+8 QTPX, 8 LDLWS => 6 NVZPS
+2 QPWF, 1 PRWSM => 5 WHWF
+1 QPWF, 8 LDLWS => 5 LZBQ
+127 ORE => 1 MDPJB
+4 WHWF => 4 KQHW
+1 QBCKX, 3 TMTH => 4 WLFTZ
+15 NPMPT, 4 TMTH => 6 QFBC
+12 MDPJB => 9 PRWSM
+5 QXHFH, 3 LCDVR, 24 MWFP, 1 MSFV, 1 BPDJL, 3 LQGXD, 2 DVGW => 2 KCPSH
+6 FPZXN, 1 FQSK, 3 TMTH => 1 FBHW
+25 PRWSM => 1 MGHZ
+6 XWKXC, 5 TMTH, 1 PZTGX => 6 NTQZ
+3 BPDJL, 3 DJWCL, 2 JZKMJ => 7 MWFP
+5 JFJZH => 3 DJWCL
+22 WRNJ, 12 TRSK => 5 TBGJC
+3 HKWP => 1 PDRN
+3 JWQH => 5 JZKMJ
+4 WBQF => 2 BJNS
+1 GNBQ => 9 FQSK
+8 HMVG, 1 HQHD => 5 NJFNC
+7 QBCKX, 1 FQSK => 9 NDCQ
+3 XWKXC, 7 QFBC, 3 GPFRS, 2 LPQZ, 2 LQGXD, 20 LZKM, 1 QRTH => 8 TDTKT
+1 QTPX => 3 LPQZ
+2 QGVQC, 14 LDLWS => 1 NPMPT
+1 QRTH, 7 BPDJL => 7 XWKXC
+9 WLFTZ, 8 TDLQ => 6 GKPK
+4 GNBQ => 3 QXHFH
+3 TBGJC, 1 LPQZ => 3 DVGW
+3 NDCQ, 1 KGZT => 7 FPZXN
+36 WLFTZ, 1 KCPSH, 1 GKPK, 1 TDTKT, 3 CSPFK, 27 JZKMJ, 5 VGLV, 39 XWKXC => 1 FUEL
+115 ORE => 7 QGVQC
+21 NTQZ, 11 HQHD, 33 JFJZH, 3 NJFNC, 3 MSFV, 1 TRSK, 7 WRNJ => 9 CSPFK
+3 DVGW => 4 TDLQ
+5 FPZXN => 6 WRNJ
+10 TSDLM, 17 XDKP, 3 PDRN => 2 HQHD
+1 PCWS => 3 PZTGX
+2 QXHFH => 5 JWQH
+17 KQHW => 2 WBQF
+139 ORE => 5 LDLWS
+3 TSDLM => 9 KGZT
+16 NPMPT => 3 QTPX
+3 DVGW, 5 KVFMS, 3 WLFTZ => 6 GPFRS
+1 PZTGX, 2 LCDVR, 13 TBGJC => 6 LZKM
+5 ZXVNM, 2 QXHFH => 4 MSFV
+4 XDKP, 7 FBHW, 2 PCWS => 3 LCDVR
+3 TRSK => 7 KVFMS
+10 LDLWS => 9 TMTH
+2 TBGJC => 6 LQGXD
+2 TRSK => 6 ZXVNM
+4 KQHW, 1 NVZPS => 8 ZLDQF
+2 LZBQ => 4 QBCKX
+7 QBCKX => 6 TSDLM
+152 ORE => 3 QPWF
+2 TSDLM, 8 WHWF => 3 HKWP
+19 FQSK => 8 QRTH
+19 QTPX => 3 GNBQ
+4 PDRN, 12 HKWP, 4 PCWS => 3 XDKP
+6 LZBQ, 19 BJNS => 5 BPDJL
+5 HKWP, 6 NVZPS => 3 PCWS';
+
+$inputParsed = explode(PHP_EOL, $input);
+
+$chemicals = [];
+
+foreach ($inputParsed as $in) {
+   [$reactionParts, $chemicalsParts] = explode("=>", $in);
+   [$qty, $name] = explode(" ", trim($chemicalsParts));
+   $reactions = explode(",", trim($reactionParts));
+
+   $chemical = [
+      'qty' => $qty,
+      'reactions' => []
+   ];
+   foreach ($reactions as $reaction) {
+
+      [$reactionQty, $reactionName] = explode(" ", trim($reaction));
+      $chemical['reactions'][$reactionName] = $reactionQty;
+   }
+   $chemicals[$name] = $chemical;
+}
+
+$make = [ 'FUEL' => true ];
+$lvl = [ 'FUEL' => 0 ];
+
+while (true) {
+   $key = key($make);
+   if (!isset($chemicals[$key])) {
+      $chemicals[$key] = ['reactions' => []];
+   }
+   foreach ($chemicals[$key]['reactions'] as $cc => $r) {
+      if (!isset($lvl[$cc])) {
+         $lvl[$cc] = 0;
+      }
+      $lvl[$cc]++;
+      $make[$cc] = true;
+   }
+   $b = next($make);
+   if (!$b) {
+      break;
+   }
+}
+
+$l = 0;
+$r = 1000000000000;
+
+while ($l < $r) {
+   $n = ceil(($l+$r)/2);
+
+   $ans = getRequireORE($n);
+   if ($ans > 1000000000000) {
+      $r = $n-1;
+   } else {
+      $l = $n;
+   }
+}
+
+echo $l;
+
+function getRequireORE($count) {
+   global $chemicals, $lvl;
+
+   $l = $lvl;
+
+   $need = [ 'FUEL' => $count ];
+
+   while (true) {
+
+      foreach ($l as $key => $v) {
+         if ($v === 0) {
+
+            if ($key === 'ORE') {
+               return $need[$key];
+            }
+
+            foreach ($chemicals[$key]['reactions'] as $c => $vv) {
+
+               $perBatch = $chemicals[$key]['qty'];
+
+               if (!isset($need[$c])) {
+                  $need[$c] = 0;
+               }
+               $need[$c] += $vv * ceil(($need[$key])/$perBatch);
+               $l[$c]--;
+            }
+            unset($l[$key]);
+            break;
+         }
+
+      }
+   }
+   return 0;
+}
+
