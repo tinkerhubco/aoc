79772dc6fed22ccc37f41906d0103702d0eec037
arthurvicencio <artsvicencio@gmail.com>
2021-12-12 17:20:51 +0800

    Create part-2.go

---
+++ 2021/day-7/arthurvicencio/part-2.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+const MaxUint = ^uint(0)
+const MaxInt = int(MaxUint >> 1)
+
+func main() {
+
+	inputFile, _ := ioutil.ReadFile("input.txt")
+
+	parsedInput := strings.Split(string(inputFile), ",")
+
+	nums := make([]int, 0)
+	for _, v := range parsedInput {
+		n, _ := strconv.Atoi(v)
+		nums = append(nums, n)
+	}
+
+	least := MaxInt
+	for i := range nums {
+		fuel := 0
+		for _, v := range nums {
+			dist := abs(v - i)
+			fuel += triangleNumber(dist)
+		}
+		if fuel < least {
+			least = fuel
+		}
+	}
+
+	fmt.Println(least)
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func triangleNumber(n int) int {
+	return (n * (n + 1)) / 2
+}
